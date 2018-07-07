7bbc227db4dfdc5aa8047bab4a40f77a9e9dc40b
freester1 <arvinshma@gmail.com>
2018-07-07 10:47:01 -0400

    correct style

--- c_src/hiredis.c
+++ c_src/hiredis.c
@@ -9,17 +9,17 @@
    (ERL_NIF_MAJOR_VERSION == 2 && ERL_NIF_MINOR_VERSION >= 10))
 
 // State
-static pthread_mutex_t *hiredis_lock;
-static redisContext *redis_context = NULL;
-static redisReply *redis_reply = NULL;
-static char *hostname = "127.0.0.1";
+static pthread_mutex_t* hiredis_lock;
+static redisContext* redis_context = NULL;
+static redisReply* redis_reply = NULL;
+static char* hostname = "127.0.0.1";
 static int port = 6379;
 
 // API
 //
 // query :: String -> String
 // takes in a redis command string `SET FOO BAR` and returns the result
-static ERL_NIF_TERM query(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
+static ERL_NIF_TERM query(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
   pthread_mutex_lock(hiredis_lock);
   ErlNifBinary ex_query_str;
 
@@ -29,7 +29,7 @@ static ERL_NIF_TERM query(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
     return enif_make_badarg(env);
   }
 
-  char *c_query_str = strndup((char *)ex_query_str.data, ex_query_str.size);
+  char *c_query_str = strndup((char*)ex_query_str.data, ex_query_str.size);
 
   redis_reply = redisCommand(redis_context, c_query_str);
   char result[redis_reply->len + 1];
@@ -48,7 +48,7 @@ static ErlNifFunc funcs[] = {{"query", 1, query, ERL_NIF_DIRTY_JOB_IO_BOUND}};
 static ErlNifFunc funcs[] = {{"query", 1, query}};
 #endif
 
-static int load(ErlNifEnv *env, void **priv, ERL_NIF_TERM info) {
+static int load(ErlNifEnv* env, void** priv, ERL_NIF_TERM info) {
   if (redis_context)
     return 0;
 
@@ -70,15 +70,15 @@ static int load(ErlNifEnv *env, void **priv, ERL_NIF_TERM info) {
   return 0;
 }
 
-static int reload(ErlNifEnv *env, void **priv, ERL_NIF_TERM info) { return 0; }
+static int reload(ErlNifEnv* env, void** priv, ERL_NIF_TERM info) { return 0; }
 
-static int upgrade(ErlNifEnv *env, void **priv, void **old_priv,
+static int upgrade(ErlNifEnv* env, void** priv, void** old_priv,
                    ERL_NIF_TERM info) {
 
   return load(env, priv, info);
 }
 
-static void unload(ErlNifEnv *env, void *priv) {
+static void unload(ErlNifEnv* env, void* priv) {
   if (redis_context)
     redisFree(redis_context);
   if (redis_reply)
