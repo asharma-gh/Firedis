a8ea718f707bd5352857edadf77ae5c4da3eb4d5
Arvin Sharma <arvin@bookbub.com>
2018-07-06 22:26:12 -0400

    partial impl

--- c_src/hiredis.c
+++ c_src/hiredis.c
@@ -5,15 +5,35 @@
 #define HAS_DIRTY_SCHEDULER (ERL_NIF_MAJOR_VERSION > 2 || (ERL_NIF_MAJOR_VERSION == 2 && ERL_NIF_MINOR_VERSION >= 10))
 
 
+// State
 static redisContext* redis_context = NULL;
 static redisReply* redis_reply = NULL;
 static char* hostname = "127.0.0.1";
 static int port = 6379;
 
 // API
+//
+// query :: String -> String
+// takes in a redis command string `SET FOO BAR` and returns the result
 static ERL_NIF_TERM query(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
+  
+  ErlNifBinary ex_query_str;
 
-  return 0;
+  // if input is not a string error
+  if (enif_inspect_iolist_as_binary(env, argv[0], &ex_query_str)) {
+    return enif_make_badarg(env);
+  }
+
+  char* c_query_str = strndup((char*)ex_query_str.data, ex_query_str.size);
+
+  redis_reply = redisCommand(redis_context, c_query_str);
+  char result[redis_reply->len + 1];
+  memcpy(result, redis_reply->str, redis_reply->len + 1);
+  
+  freeReplyObject(redis_reply);
+  free(c_query_str);
+
+  return enif_make_string(env, result, ERL_NIF_LATIN1);
 }
 
 // NIF Boilerplate
@@ -30,6 +50,21 @@ static ErlNifFunc funcs[] = {
 static
 int
 load(ErlNifEnv* env, void** priv, ERL_NIF_TERM info) {
+  if (redis_context)
+    redisFree(redis_context);
+  
+  struct timeval timeout = { 0, 250000 }; // .25 seconds
+  redis_context = redisConnectWithTimeout(hostname, port, timeout);
+
+  if (redis_context == NULL || redis_context->err) {
+    if (redis_context) {
+        printf("Connection error: %s\n", redis_context->errstr);
+        redisFree(redis_context);
+    } else {
+        printf("Connection error: can't allocate redis context\n");
+        return 1;
+    }
+  }
 
   return 0;
 }
