9182bc15ae0f4b171a15a72c1f886c64d3a012a7
William Tan <williamjefftan@gmail.com>
2018-07-08 12:27:55 -0400

    Implement checks to see if redis server is running
    Additional commands will be added later to set up the redis instance
    with data for testing

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,24 @@
 ExUnit.start()
+
+cmds = [
+  ["ping"]
+]
+
+
+Enum.each(cmds, fn args ->
+  {output, status} = System.cmd("redis-cli", args, stderr_to_stdout: true)
+  if status !=0 do
+    IO.puts """
+    Command:
+
+    redis-cli #{Enum.join(args, " ")}
+
+    error'd with:
+
+    #{output}
+
+    Please verify the redis server is running and listening on 127.0.0.1:6379 with no authentication.
+    """
+    System.halt(1)
+  end
+end)
